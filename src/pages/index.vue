a5876062b240d23f6012494120d37d32baf74f52
Théo Ballerini <theo.ballerini@divcom.ch>
2026-03-11 11:41:08 +0100

    ajout API Jikan

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <h1 class="text-h4 my-4">Personnages Rick & Morty</h1>
+    <h1 class="text-h4 my-4">Liste anime</h1>
 
     <!-- Chargement (skeleton) -->
     <v-row v-if="loading">
@@ -14,10 +14,28 @@
       {{ error }}
     </v-alert>
 
-    <!-- TODO : Liste des personnages -->
+    <!-- TODO : Liste des animes -->
     <v-row v-else>
-      <v-col cols="12">
-        <p>{{ characters.length }} personnage(s) chargé(s)</p>
+      <v-col
+          v-for="anime in animes"
+          :key="anime.id"
+          cols="12" sm="6" md="4" lg="3"
+      >
+        <v-card class="h-100">
+          <v-img :src="anime.images.webp.large_image_url" :alt="anime.title" height="400" cover />
+          <v-card-title>{{ anime.title }}</v-card-title>
+          <v-card-text>
+            <v-chip
+                size="small"
+                class="mr-2"
+            >
+              {{ anime.episodes }} episodes
+            </v-chip>
+            <v-chip size="small" variant="outlined">
+              score : {{ anime.score }}
+            </v-chip>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -27,7 +45,7 @@
 import { ref, onMounted } from 'vue'
 
 // État réactif
-const characters = ref([])
+const animes = ref([])
 const loading = ref(true)
 const error = ref(null)
 
@@ -36,10 +54,17 @@ const error = ref(null)
 // La réponse contient { info: {...}, results: [...] }
 onMounted(async () => {
   try {
-    // TODO : fetch + vérification response.ok + extraction des résultats
+    const response = await fetch('https://api.jikan.moe/v4/anime?q=bleach&sfw')
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ${response.status}`)
+    }
+
+    const data = await response.json()
+    animes.value = data.data
 
   } catch (err) {
-    error.value = `Impossible de charger les personnages : ${err.message}`
+    error.value = `Impossible de charger les animes : ${err.message}`
   } finally {
     loading.value = false
   }
