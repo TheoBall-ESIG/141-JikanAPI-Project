b9cdf31c92bd1b074f22df2679ebe17e2b9342d5
Théo Ballerini <theo.ballerini@divcom.ch>
2026-05-20 11:26:10 +0200

    fix: au changement de page, reset le scroll sauf sur retour

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,14 @@ import { routes } from 'vue-router/auto-routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    // Si on revient en arrière (bouton retour), restaure la position
+    if (savedPosition) {
+      return savedPosition
+    }
+    // Sinon (navigation normale), remonte en haut
+    return { top: 0 }
+  }
 })
 
 export default router
